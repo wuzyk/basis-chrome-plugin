d7ca158e822a10219ef7e92fb3bf53d818f0cb20
Vladimir Ratsev <wuzykk@gmail.com>
2013-01-21 12:12:01 +0600

    update to new appcp; some refactoring

--- content.js
+++ content.js
@@ -1,31 +1,31 @@
 (function(){
-
-  var transferDiv;
-  var transferDataHandler = function(){
-    var action = transferDiv.getAttribute('action');
-    var data = transferDiv.innerText;
-
-    console.log('transfer data action:', action);
-    //console.log('transfer data:', data);
-
-    port.postMessage({ action: action, data: data });
-  }
-
   var port = chrome.extension.connect({name: "contentScriptPort"});
   port.onMessage.addListener(function(msg) {
     if (msg.action == 'pageScriptInited')
-    {
-      transferDiv = document.getElementById('transferDiv');
-      if (transferDiv)
-      {
-        transferDiv.addEventListener('transferData', transferDataHandler);
-        port.postMessage({ action: 'transportInited' });
-      }
-    }
+      initTransport();
   });
-
   port.postMessage({ action: 'contentScriptInited' });
 
-  console.log('content script init');
+  function initTransport(){
+    var transferDiv = document.getElementById('transferDiv');
+    if (transferDiv)
+    {
+      transferDiv.addEventListener('transferData', function(){
+        var action = transferDiv.getAttribute('action');
+        var data = transferDiv.innerText;
 
+        console.log('transfer data action:', action);
+        //console.log('transfer data:', data);
+
+        sendMessage({ action: action, data: data });
+      });
+      sendMessage({ action: 'ready' });
+    }
+  }
+
+  function sendMessage(message){
+    port.postMessage(message);
+  }
+
+  console.log('content script inited');
 })();
--- createTab.js
+++
@@ -1,6 +0,0 @@
-  
-  chrome.devtools.panels.create(
-    "basis.js",
-    "icon32x32.png",
-    "app-control-panel/index.html"
-  );
